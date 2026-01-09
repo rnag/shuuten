1a388a5911d7f6c84c903ff2637a932f950470f0
Nag, Ritvik <nagrim7@gmail.com>
2026-01-09 10:52:53 -0500

    updates

--- src/shuuten/_cli.py
+++ src/shuuten/_cli.py
@@ -3,8 +3,6 @@
 import typer
 from rich.console import Console
 
-from shuuten import utils
-
 app = typer.Typer()
 console = Console()
 
@@ -15,7 +13,6 @@ def main():
     console.print("Replace this message by putting your code into "
                "shuuten.cli.main")
     console.print("See Typer documentation at https://typer.tiangolo.com/")
-    utils.do_something_useful()
 
 
 if __name__ == "__main__":
